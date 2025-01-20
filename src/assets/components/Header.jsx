d46980d522ac9e3db3fa4bb41c4d67e874635578
PatrizioFiori <patrizio.fiori1995@gmail.com>
2025-01-20 17:07:07 +0100

    Struttura principale (add: bootstrap)

---
+++ src/assets/components/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <div className='bg-dark-subtle text-center py-3'>
+            <h1>Blog</h1>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/assets/components/Main.jsx
@@ -0,0 +1,11 @@
+
+
+const Main = () => {
+    return (
+        <div className='bg-info-subtle text-center py-3'>
+            <p>Main</p>
+        </div>
+    )
+}
+
+export default Main
